5fdf6c43fb218b69f40b28258e0435254f9b500c
Ilyas Azizi <57682191+ilyasazizi@users.noreply.github.com>
2019-11-21 13:24:18 -0500

    Add files via upload

---
+++ Python Design Project.py
@@ -0,0 +1,11 @@
+#main progam file #run
+from AziziFunctions import*
+
+jump(0,200)
+
+
+#The "L" In LV
+
+#Top part of the "L"
+
+bob.write("Let's Learn Python!", align="center", font=(None, 16, "bold"))
